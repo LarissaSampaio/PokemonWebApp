f61c10efe3a0db4dbe8269191cc8a13f773f0c5c
LarissaSampaio <larisampaio@hotmail.com>
2021-10-21 22:41:01 -0300

    Adicionar arquivos de projeto.

---
+++ PokemonWebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PokemonWebApp.Models;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace PokemonWebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        string baseURL = "https://localhost:44395/";
+
+        public async Task OnGetAsync()
+        {
+            List<Pokemon> pokemons = new List<Pokemon>();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseURL);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync("api/Pokemons");
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = response.Content.ReadAsStringAsync().Result;
+                    pokemons = JsonConvert.DeserializeObject<List<Pokemon>>(result);
+                }
+            }
+
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
